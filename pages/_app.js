031b06519aa3e90f5fc85f639abd5d491156e6fa
Simone <startae14@gmail.com>
2022-01-08 19:05:12 +0100

    Remove useless code and add footer content and some style

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,15 @@ function MyApp({ Component, pageProps }) {
       <Component {...pageProps} />
       <footer>
         <p>
+          <a href="mailto:info@movestudio.it?subject=I need infos...">
+            info@movestudio.it
+          </a>
+        </p>
+        <p>
           All rights reserved©2022
+        </p>
+        <p>
+          Designed with <span className="footer-heart">&hearts;</span> by MoveStudio
         </p>
       </footer>
     </>
