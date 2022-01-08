6bb5e6c38cc5d1caeee60ee39678a4ae46980dc2
Simone <startae14@gmail.com>
2022-01-08 18:37:39 +0100

    Create the footer in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <footer>
+        <p>
+          All rights reserved©2022
+        </p>
+      </footer>
+    </>
+  );
 }
 
 export default MyApp
