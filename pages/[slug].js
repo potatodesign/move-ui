934ce3fea360cfc1db9af1e587a475d0d6553237
Simone <startae14@gmail.com>
2022-01-08 19:46:45 +0100

    retrieve the slug value in the dynamic page

--- pages/[slug].js
+++ pages/[slug].js
@@ -5,14 +5,15 @@ import styles from '../styles/Work.module.css'
 
 const Work = () => {
   const router = useRouter()
+  const { slug } = router.query
   return (
     <div className={styles.container}>
     <Head>
-    <title>Move studio works {router.query.id}</title>
-    <meta name="description" content={`Move studio work ${router.query.id}}`} />
+    <title>Move studio works {slug}</title>
+    <meta name="description" content={`Move studio work ${slug}}`} />
     <link rel="icon" href="/favicon.ico" />
   </Head>
-      Page individual work {router.query.id}
+      Page individual work {slug}
     </div>
   );
 };
